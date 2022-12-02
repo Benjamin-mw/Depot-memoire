3300331b1552fbcb20f9d9cc16d6a2347f2ebb57
Benjamin Moinard <benjamin.moinard@dauphine.eu>
2022-12-02 16:41:57 +0100

    Ceci permet normalement d'installer les données. parfois, il faut directement se rendre sur le site via l'url et installer manuellement le package

---
+++ Installation des packages et chargement des données.R
@@ -0,0 +1,17 @@
+#install.packages("CASdatasets", repos = "http://dutangc.perso.math.cnrs.fr/RRepository/", type="source")
+#install.packages("CASdatasets", repos = "http://dutangc.free.fr/pub/RRepos/", type="source")
+install.packages("CASdatasets", repos = "http://cas.uqam.ca/pub/", type="source")
+
+#library(CASdatasets)
+install.packages("rpart")
+install.packages("partykit")
+library(rpart)
+library(partykit)
+install.packages("devtools")
+devtools::install_github("dutangc/CASdatasets", subdir="pkg")
+library(CASdatasets)
+
+
+install.packages("xts")
+install.packages("sp")
+install.packages("zoo")
